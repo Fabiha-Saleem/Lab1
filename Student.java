d6f6a62193f535851ebeae006c765dd9608468b4
Fabiha-Saleem <pwrettisfabiih@gmail.com>
2025-09-10 03:41:30 -0700

    Fabiha Saleem Task2 OOPs Lab

--- Student.java
+++ Student.java
@@ -1,11 +1,38 @@
- // Fabiha Saleem
-// Sp25-bcs-034
- class Student{
- private string name ;
- private string email;
- private double cgpa;
- 
+ public class Student{
+private String name;
+ private String email;
+private double cgpa;
+
+public Student(String name, String email, double cgpa){
+this.name=name;
+this.email=email;
+this.cgpa=cgpa;
+}
+public String getName(){
+return name;
+}
+public String getemail(){
+return email;
+}
+public double getCgpa(){
+return cgpa;
+}
+
+public void setName(String name){
+this.name=name;
+}
+public void setEmail(String email){
+this.email=email;
+}
+public void setcgpa(double cgpa){
+this.cgpa=cgpa;
+}
+
 public void display(){
-system.out.print("Hello");
+System.out.println("Student Name"+name);
+System.out.println("Student email"+email);
+System.out.println("Student cgpa"+cgpa);
 }
 }
+
+
--- StudentTest.java
+++ StudentTest.java
@@ -1,6 +1,11 @@
 public class StudentTest{
- public static void main(string args[]){
-Student s1 = new Student;
+public static voidmain(String args[]){
+Student s1= new Student("Fabiha" , "fabi@gmail.com", 3.9);
+s1.display();
+System.out.println("Updates");
+s1.setName("Sania");
+s1.setEmail("sania@gmail.com");
+s1.setCgpa("4.0");
 s1.display();
 }
 }
