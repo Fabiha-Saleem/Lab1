bc42171f6724d9ee2a3649511de8d5f0596fda1d
Fabiha-Saleem <pwrettisfabiih@gmail.com>
2025-09-10 03:40:20 -0700

    Task1

---
+++ Student.java
@@ -0,0 +1,11 @@
+ // Fabiha Saleem
+// Sp25-bcs-034
+ class Student{
+ private string name ;
+ private string email;
+ private double cgpa;
+ 
+public void display(){
+system.out.print("Hello");
+}
+}
---
+++ StudentTest.java
@@ -0,0 +1,6 @@
+public class StudentTest{
+ public static void main(string args[]){
+Student s1 = new Student;
+s1.display();
+}
+}
